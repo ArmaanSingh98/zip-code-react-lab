f01f0239331fbc403e6d89ca7f000065da2d7cde
ArmaanSingh98 <armaandsingh@gmail.com>
2021-09-28 21:03:18 -0400

    Edited The app to be more based on todays react lab lecture

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 
 function City(props) {
-  return (<div id>
-    <div id>
+  return (<div>
+    <div>
       {props.cityLocation}
     </div>
     <ul>
     <li>State: {props.state} </li>
-    <li>Location: {props.locationCoords} </li>
+    <li>Location: {props.locationCoordsLat + ',' + props.locationCoordsLong} </li>
     <li>Population (estimated): {props.population} </li>
     <li>Total Wages: {props.totalWages} </li>
     </ul>
@@ -19,7 +19,7 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div id>
+  return (<div>
     <div>Zip Code: </div>
     <input type = "text" onChange= {props.changeHandler} value = {props.zipCode}/>
     </div>
@@ -31,42 +31,51 @@ class App extends Component {
 
   state = {
     zipCode: '',
-    data: [],
+    cities: [],
   }
 
  zipChanged = (event) => {
-   let length = event.target.event.length
-   if(length < 6)
-   {
-     this.setState({zipCode: event.target.value, data:[]})
-     if(length === 5)
-     {
-      fetch('http://ctp-zip-api.herokuapp.com/zip/' + event.target.value)
-      .then((res) => res.json())
-      .then((data) => this.setState({data:data}))
-     }
-   }
- }
+  console.log(event.target.value)
+  this.setState({zipCode: event.target.value})
+  if(event.target.value.length === 5)
+  {      
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+  this.setState({cities:data})
+  })
+  .catch(err =>{
+    console.log('No results')
+    this.setState({cities: [] })
+  })}
+  else{
+    this.setState({cities: [] })
+  }
+
+    }
 
   render() {
-    console.log(this.state.data)
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
         <ZipSearchField zipCode ={this.state.zipCode} changeHandler = {this.zipChanged} />
+        <div>Current Zip is {this.state.zipCode}</div>
         <div>
           {
-            this.state.data.map((info , i) => (
-            <City key = {i} 
-            state = {info.State}
-            cityLocation = {info.Location}
-            locationCoords = {info.Lat + info.Long}
-            population = {info.EstimatedPopulation}
-            totalWages = {info.TotalWages}
+            this.state.cities.map((city) => 
+            <City data = {city} cityName = {city.City}
+            state = {city.State}
+            population = {city.EstimatedPopulation}
+            totalWages = {city.TotalWages}
+            locationCoordsLat ={city.Lat}
+            locationCoordsLong = {city.Long}
+            cityLocation = {city.LocationText}
             />
-            ))}
+            )}
+            {this.state.cities.length === 0 ? <h1>No Results</h1>:null}
         </div>
       </div>
     );
